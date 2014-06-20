6996bc741e46bc7f662b42d35def015937a980c8
Dennis Heckman <dennis.heckman@vertive.com>
2014-06-20 16:42:01 -0500

    add initial user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'index/index'
+  get 'user' => 'user#index'
+  post 'user' => 'user#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
