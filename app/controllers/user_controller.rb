85583e63d6223e3dedd9af279693c24384af4a54
Dennis Heckman <dennis.heckman@vertive.com>
2014-07-16 20:50:33 -0500

    users routing and view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
 class UserController < ApplicationController
   def index
   end
+
+  def show
+    #render plain: params[:user].inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'user' => 'user#index'
-  post 'user' => 'user#create'
+  post 'user' => 'user#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
